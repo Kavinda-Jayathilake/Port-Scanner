d0e95c3cd11592d7cbe8b8a9a8f597dbd63a0420
Kavi <imalkakavinda29785@gmail.com>
2025-12-24 17:57:03 +0530

    first version, simple loop+threads

--- port_scanner.py
+++ port_scanner.py
@@ -1,15 +1,22 @@
-#!/usr/bin/env python3
 import socket
 import threading
+import time
 
 def scan(port):
-    pass
-
-def workers():
-    pass
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)  # ipv4 and tcp / even i don't mention default is this
+    result = sock.connect_ex(('localhost',port))
+    if result == 0:
+        print(f"port {port} is open")
 
 def main():
-    pass
+    threads = []
+    for i in range(1,1001):
+        while(threading.active_count()>=100):
+            time.sleep(0.01)
+        t = threading.Thread(target=scan,args=(i,))
+        t.start()
+        threads.append(t)
+    for thread in threads: thread.join()
 
 if __name__ == '__main__':
     main()
