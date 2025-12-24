5fe34ca52f712b8c4b83f8c8e0959439836cc9c2
Kavi <imalkakavinda29785@gmail.com>
2025-12-24 20:05:12 +0530

    updated to accept terminal inputs

--- port_scanner.py
+++ port_scanner.py
@@ -1,30 +1,58 @@
 import socket
 import threading
-import time
+import sys
 from queue import Queue
+import re
 
-def scan(q):
+def scan(q,ip):
     while True:
         try:
             port = q.get_nowait()
         except:
             break
         sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        sock.settimeout(0.3)  # may be bad connection + 0.3s will not trigger a port. feel free to make 0.5-1s
-        result = sock.connect_ex(("localhost",port))
+        sock.settimeout(1)  # may be bad connection + 0.3s will not trigger a port. feel free to make 0.5-1s
+        result = sock.connect_ex((ip,port))
         if result == 0:
             print(f"port {port} is open")
         q.task_done()
 
 def main():
-    q = Queue()
-    for i in range(1,1001): q.put(i)
-    threads = []
-    for i in range(100):
-        t = threading.Thread(target=scan,args=(q,))
-        t.start()
-        threads.append(t)
-    for thread in threads: thread.join()
+    if len(sys.argv) < 4:
+        print(f"Usage python3 file_path <ip> <start_port> <end_port>")
+    else:
+        try:
+            ip = sys.argv[1]
+            pattern = r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+            match = re.match(pattern,ip)
+            if not match:
+                raise ValueError("IP address should be in x.x.x.x form")
+            start = int(sys.argv[2])
+            end = int(sys.argv[3])
+            if start<1 or end<1:
+                raise ValueError("ports must be greater than 0")
+            elif start>65535 or end>65535:
+                raise ValueError("ports must be lower than 65535")
+            elif start>end:
+                raise ValueError("start must be lower than or equal to end")
+        except ValueError as e:
+            print(f"Error : {e}")
+            print(f"Usage python3 file_path <ip> <start_port> <end_port>")
+            sys.exit(1)
+        except Exception as e:
+            print(e)
+            sys.exit(1)
+
+        q = Queue()
+        for i in range(start,end+1): q.put(i)
+        threads = []
+        for i in range(100):
+            t = threading.Thread(target=scan,args=(q,ip))
+            t.start()
+            threads.append(t)
+        for thread in threads: thread.join()
+        print("Scan is completed")
+    
 
 if __name__ == '__main__':
     main()
