607e18d643f362791efc657737e6b86c0df553cf
Kavi <imalkakavinda29785@gmail.com>
2025-12-24 18:57:56 +0530

    optimized with queue.scans little bit faster

--- port_scanner.py
+++ port_scanner.py
@@ -1,19 +1,27 @@
 import socket
 import threading
 import time
+from queue import Queue
 
-def scan(port):
-    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)  # ipv4 and tcp / even i don't mention default is this
-    result = sock.connect_ex(('localhost',port))
-    if result == 0:
-        print(f"port {port} is open")
+def scan(q):
+    while True:
+        try:
+            port = q.get_nowait()
+        except:
+            break
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sock.settimeout(0.3)  # may be bad connection + 0.3s will not trigger a port. feel free to make 0.5-1s
+        result = sock.connect_ex(("localhost",port))
+        if result == 0:
+            print(f"port {port} is open")
+        q.task_done()
 
 def main():
+    q = Queue()
+    for i in range(1,1001): q.put(i)
     threads = []
-    for i in range(1,1001):
-        while(threading.active_count()>=100):
-            time.sleep(0.01)
-        t = threading.Thread(target=scan,args=(i,))
+    for i in range(100):
+        t = threading.Thread(target=scan,args=(q,))
         t.start()
         threads.append(t)
     for thread in threads: thread.join()
